ca50f7f7b7afa16642f42696f2be0ec6d3616275
bscs24133 <bscs24133@itu.edu.pk>
2026-05-07 21:31:30 +0500

    Config file with dataset paths

---
+++ src/config.py
@@ -0,0 +1,14 @@
+# src/config.py
+
+STATIC_TRAIN   = r'C:\Users\User\Desktop\pyProj\PSL-to-speech-project\Dataset\1_UAlpha40_Mendeley\static_signs\train'
+STATIC_TEST    = r'C:\Users\User\Desktop\pyProj\PSL-to-speech-project\Dataset\1_UAlpha40_Mendeley\static_signs\test'
+DYNAMIC_RAW    = r'C:\Users\User\Desktop\pyProj\PSL-to-speech-project\Dataset\1_UAlpha40_Mendeley\dynamic_signs\raw_videos'
+WORD_TRAIN     = r'C:\Users\User\Desktop\pyProj\PSL-to-speech-project\Dataset\2_PSL_Dictionary_Toolkit\train'
+WORD_TEST      = r'C:\Users\User\Desktop\pyProj\PSL-to-speech-project\Dataset\2_PSL_Dictionary_Toolkit\test'
+FLOW_OUTPUT    = r'C:\Users\User\Desktop\pyProj\PSL-to-speech-project\Dataset\3_OpticalFlow'
+
+MODEL_SAVE_DIR = r'models/'
+IMG_SIZE       = (64, 64)
+N_FRAMES       = 16
+BATCH_SIZE     = 16
+EPOCHS         = 30
