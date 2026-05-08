e266dcaeca4ecbc0ac7cfb0ea241ba3ec5d79049
rabia-syed-103 <bscs24103@itu.edu.pk>
2026-05-08 10:18:53 +0500

    Changed Path According to me

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 # src/config.py
 
-STATIC_TRAIN   = r'C:\Users\User\Desktop\pyProj\PSL-to-speech-project\Dataset\1_UAlpha40_Mendeley\static_signs\train'
-STATIC_TEST    = r'C:\Users\User\Desktop\pyProj\PSL-to-speech-project\Dataset\1_UAlpha40_Mendeley\static_signs\test'
-DYNAMIC_RAW    = r'C:\Users\User\Desktop\pyProj\PSL-to-speech-project\Dataset\1_UAlpha40_Mendeley\dynamic_signs\raw_videos'
-WORD_TRAIN     = r'C:\Users\User\Desktop\pyProj\PSL-to-speech-project\Dataset\2_PSL_Dictionary_Toolkit\train'
-WORD_TEST      = r'C:\Users\User\Desktop\pyProj\PSL-to-speech-project\Dataset\2_PSL_Dictionary_Toolkit\test'
-FLOW_OUTPUT    = r'C:\Users\User\Desktop\pyProj\PSL-to-speech-project\Dataset\3_OpticalFlow'
+STATIC_TRAIN   = r'D:\Rabia_Uni\Semester_4\Programming_for_AI\Projects\Project_2\Dataset\1_UAlpha40_Mendeley\static_signs\train'
+STATIC_TEST    = r'D:\Rabia_Uni\Semester_4\Programming_for_AI\Projects\Project_2\Dataset\1_UAlpha40_Mendeley\static_signs\test'
+DYNAMIC_RAW    = r'D:\Rabia_Uni\Semester_4\Programming_for_AI\Projects\Project_2\Dataset\1_UAlpha40_Mendeley\dynamic_signs\raw_videos'
+WORD_TRAIN     = r'D:\Rabia_Uni\Semester_4\Programming_for_AI\Projects\Project_2\Dataset\2_PSL_Dictionary_Toolkit\train'
+WORD_TEST      = r'D:\Rabia_Uni\Semester_4\Programming_for_AI\Projects\Project_2\Dataset\2_PSL_Dictionary_Toolkit\test'
+FLOW_OUTPUT    = r'D:\Rabia_Uni\Semester_4\Programming_for_AI\Projects\Project_2\Dataset\3_OpticalFlow'
 
 MODEL_SAVE_DIR = r'models/'
 IMG_SIZE       = (64, 64)
