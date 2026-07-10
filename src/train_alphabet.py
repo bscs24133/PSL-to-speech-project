2ff6a3a559687e73ee833dea7f560b17db1bf9db
rabia-syed-103 <bscs24103@itu.edu.pk>
2026-07-10 23:02:32 +0500

    Pushed

--- src/train_alphabet.py
+++ src/train_alphabet.py
@@ -28,16 +28,19 @@ def build_alphabet_cnn(n_classes=36):
         layers.Conv2D(32, (3,3), activation='relu', padding='same'),
         layers.MaxPooling2D((2,2)),
         layers.Dropout(0.25),
+        
         layers.Conv2D(64, (3,3), activation='relu', padding='same'),
         layers.BatchNormalization(),
         layers.Conv2D(64, (3,3), activation='relu', padding='same'),
         layers.MaxPooling2D((2,2)),
         layers.Dropout(0.25),
+        
         layers.Conv2D(128, (3,3), activation='relu', padding='same'),
         layers.BatchNormalization(),
         layers.Conv2D(128, (3,3), activation='relu', padding='same'),
         layers.MaxPooling2D((2,2)),
         layers.Dropout(0.25),
+        
         layers.GlobalAveragePooling2D(),
         layers.Dense(256, activation='relu'),
         layers.BatchNormalization(),
